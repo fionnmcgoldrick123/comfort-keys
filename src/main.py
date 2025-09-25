214263b8b84f74a1d9ee5aee9ce1647f380b9e5a
fionn mcgoldrick <fionnmc123@gmail.com>
2025-09-25 21:30:31 +0100

    Application header added. Comments to seperate code segments.

--- src/main.py
+++ src/main.py
@@ -8,21 +8,27 @@ from PySide6.QtWidgets import (
     QLabel         # simple text label
 )
 
+from PySide6.QtCore import Qt 
+
 app = QApplication(sys.argv)
 
 window = QMainWindow()
-window.setWindowTitle("My First PySide6 App")
+window.setWindowTitle("Comfort Keys")
 
 #central widget / layout
 central = QWidget()
 layout = QVBoxLayout()
 central.setLayout(layout)
 
-label = QLabel("Hello, PySide6!")
-layout.addWidget(label)
+#centered header for application
+header = QLabel("comfort-keys")
+header.setStyleSheet("font-size: 24px;")
+header.setAlignment(Qt.AlignHCenter | Qt.AlignTop)
+layout.addWidget(header)
 
-
+#window configuration
 window.setCentralWidget(central)
+window.setFixedSize(400,200)
 window.show()
 
 sys.exit(app.exec())
