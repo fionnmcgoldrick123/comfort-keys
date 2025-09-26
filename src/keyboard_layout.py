d6b86585d7f01504a040ea14f096f12390d74f08
fionn mcgoldrick <fionnmc123@gmail.com>
2025-09-26 09:12:16 +0100

    feat(keyboard_layout, mouse_layout, set_sound): Linked set_sound function to keyboard QCombo . Slight change in style.

--- src/keyboard_layout.py
+++ src/keyboard_layout.py
@@ -2,6 +2,8 @@ from PySide6.QtWidgets import QWidget, QVBoxLayout, QLabel, QComboBox
 from PySide6.QtCore import Qt
 from PySide6.QtGui import QPixmap
 
+from set_sound import set_keyboard_sound
+
 keyboard_layout = QWidget()
 left_layout = QVBoxLayout(keyboard_layout)
 left_layout.setContentsMargins(0, 0, 0, 0)
@@ -17,8 +19,9 @@ left_layout.addWidget(left_label, alignment=Qt.AlignTop | Qt.AlignHCenter)
 combo = QComboBox()
 combo.addItems(["Select Layout", "QWERTY", "AZERTY", "DVORAK", "COLEMAK"])
 combo.setCurrentIndex(-1)  # No default selection (ok if you're on Qt ≥ 6.7 with placeholder)
-combo.setStyleSheet("font-size: 16px;")
+combo.setStyleSheet("font-size: 16px; color: white;")
 combo.setPlaceholderText("Select Layout")
+combo.currentTextChanged.connect(set_keyboard_sound)
 
 # Center the COMBO too:
 left_layout.addWidget(combo, alignment=Qt.AlignTop | Qt.AlignHCenter)
---
+++ src/set_sound.py
@@ -0,0 +1,7 @@
+from PySide6.QtMultimedia import QSoundEffect
+from PySide6.QtCore import QUrl
+
+def set_keyboard_sound(sound_effect):
+    #implementation of sound effect when keyboard layout is changed
+    print("empty function called")
+    
